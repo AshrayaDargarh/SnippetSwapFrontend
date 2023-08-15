2d664ae8e5b377ebcc30a2d9a8fa2e271bb548df
AShrayaDargarh <ashraydargar01@gmail.com>
2023-08-15 20:05:56 +0530

    viteConfig

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import nodeResolve from '@rollup/plugin-node-resolve'
 
 // https://vitejs.dev/config/
 export default defineConfig(
   {
-    build: {
-      rollupOptions: {
-        plugins: [
-          nodeResolve() // add the plugin here
-        ]
-      }
-    },
   plugins: [react()],
  
 })
