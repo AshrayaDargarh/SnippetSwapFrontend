842ef22efe61d6fbc163affb7e9e51677635a3fb
AShrayaDargarh <ashraydargar01@gmail.com>
2023-08-25 22:46:31 +0530

    renderAPI

--- src/config.jsx
+++ src/config.jsx
@@ -1,3 +1,5 @@
 // export const BACKEND_API='https://snippet-swap-backend.vercel.app'
 // export const BACKEND_API='http://localhost:3002'
-export const BACKEND_API='https://snippet-swap-backend.vercel.app'
+export const BACKEND_API='https://snippetswap-api.onrender.com'
+// export const BACKEND_API='http://localhost:3002'
+// https://snippet-swap-backend.vercel.app/ 
